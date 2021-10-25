8369528c712b268cc64929e9751a8d72b7e66565
aiegoo <eozz21@gmail.com>
2021-10-26 05:38:04 +0900

    add express and template js to meadowlark js

--- webdev-node/meadowlark/site/meadowlark.js
+++ webdev-node/meadowlark/site/meadowlark.js
@@ -1,52 +1,64 @@
-import express from 'express';
-import pug from 'pug';
-import path from 'path';
+// meadowlark.js is  a travel website built using express.
+//
 
+var express = require('express');
+  
+var app = express();
 
-// console.log(express);
-const app = express();
+//set up handlebars instance for views
+var handlebars = require('express3-handlebars')
+                .create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-let fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple."
-];
+var fortunes = [
+        "Conquer your fears or they will conquer you.",
+        "Rivers need springs.",
+        "Do not fear what you don't know.",
+        "You will have a pleasant surprise.",
+        "Whenever possible, keep it simple.",
+        "Vodaphone is working on their challenges.",
+        "Express is cool with dynamic content.",
+        "I had use for the hat I got from Indonesia.",
+        "Love is a mystery."
+]; 
 
-let contents = "God is able to do all things. He lives in me";
+app.set('port', process.env.PORT || 3000);
 
-app.set('port', process.env.port || 3002);
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+// static middleware for content like CSS, images, client side JS files.
+//
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.render('home', { content: contents });
+// routes for homepage and about page.
+app.get('/', function(req, res){
+        res.render('home');
 });
 
-app.get('/about', (req, res) => {
-    let randomFortune =
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune});
+
+app.get('/about', function(req, res){
+        var randomFortune =
+                    fortunes[Math.floor(Math.random() * fortunes.length)];
+        res.render('about', {fortune: randomFortune});
 });
 
-// custom 404 page
-app.use((req, res, next) =>{
-    res.type('text/plain');
-    res.status(404);
-    res.render('500');
+
+// custom 404 page /// these are middlewares.
+app.use(function(req, res){
+        res.status(404);
+        res.render('404');
 });
 
-// custom 500 page
-app.use((err, req, res, next,) =>{
-   
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
+
+
+// custom 500 page // middlewares.
+app.use(function(err, req, res, next){
+      console.error(err.stack);
+      res.status(500);
+      res.render('500');
 });
 
-app.listen(app.get('port'), () => {
+
+app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate');
+    app.get('port') + '; press Cltr-C to terminate.');
 });
