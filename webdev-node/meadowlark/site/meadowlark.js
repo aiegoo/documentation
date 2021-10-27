c6b25f4585c69e21d43ca677af5e78cb41cb4c52
aiegoo <eozz21@gmail.com>
2021-10-27 13:23:08 +0900

    add lib/handlers.js and update package and lock.json

--- webdev-node/meadowlark/site/meadowlark.js
+++ webdev-node/meadowlark/site/meadowlark.js
@@ -1,15 +1,9 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-]
+
+const handlers = require('./lib/handlers')
+
 const app = express()
-const path = require ('path')
 
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
@@ -17,31 +11,25 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.set('view engine', 'handlebars')
 
-app.use(express.static(__dirname + '/public'))
-
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.render('home'))
+app.use(express.static(__dirname + '/public'))
 
+app.get('/', handlers.home)
 
-
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() } )
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`))
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log( `Express started on http://localhost:${port}` +
+      '; press Ctrl-C to terminate.' )
+  })
+} else {
+  module.exports = app
+}
