cd94ef634cad1b3f942ccb196dff39ba44b6ccd8
aiegoo <eozz21@gmail.com>
2021-11-08 13:56:09 +0900

    replace innerText with innerHTML

--- js/copyCode.js
+++ js/copyCode.js
@@ -2,7 +2,7 @@ const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge'
 const copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
 copyCodeButtons.forEach((copyCodeButton, index) => {
-  const code = codeBlocks[index].innerHTML  ;
+  const code = codeBlocks[index].innerHTML;
 
   copyCodeButton.addEventListener('click', () => {
     window.navigator.clipboard.writeText(code);
