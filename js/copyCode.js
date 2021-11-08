dd0d5f5b29c89db235b6a2c11a88f4e7dc4a62c2
aiegoo <eozz21@gmail.com>
2021-11-08 13:46:06 +0900

    replace innerText with innerHTML

--- js/copyCode.js
+++ js/copyCode.js
@@ -1,1 +1,16 @@
-const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');const copyCodeButtons = document.querySelectorAll('.copy-code-button');copyCodeButtons.forEach((copyCodeButton, index) => {  const code = codeBlocks[index].innerText;  copyCodeButton.addEventListener('click', () => {    window.navigator.clipboard.writeText(code);    copyCodeButton.classList.add('copied');    setTimeout(() => {      copyCodeButton.classList.remove('copied');    }, 2000);  });});
+const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');
+const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+
+copyCodeButtons.forEach((copyCodeButton, index) => {
+  const code = codeBlocks[index].innerHTML  ;
+
+  copyCodeButton.addEventListener('click', () => {
+    window.navigator.clipboard.writeText(code);
+    copyCodeButton.classList.add('copied');
+
+    setTimeout(() => {
+      copyCodeButton.classList.remove('copied');
+    }, 2000);
+  });
+});
+
