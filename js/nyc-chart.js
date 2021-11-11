6f4b034c1e6909e883c301634d39f92e34008371
aiegoo <eozz21@gmail.com>
2021-11-11 17:19:52 +0900

    fix issues with piechart script

--- js/nyc-chart.js
+++ js/nyc-chart.js
@@ -98,31 +98,31 @@ async function drawLineChart() {
 drawLineChart();
 
 const makeChart = () => {
-const data = [35, 15, 15, 35]
-const div = d3.select('body')
-const width = window.innerWidth
-const height = window.innerHeight
-const radius = Math.min(width, height) / 2
-
-const colorScale = d3.scaleOrdinal([rgb(50, 72, 34), rgb(90, 10, 30), rgb(0, 50, 20)])
-const svg = div.append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .append('g')
-    .attr('transform', 'translate(${width / 2}, ${height / 2})')
-const pie = d3.pie().value(d => d).sort(null)
-const arc = de.arc().outerRadius(radius).innerRadius(0)
-
-const g = svg.selectAll('.arc')
-  .data(pie(data))
-  .enter().append('g')
-  .attr('class', 'arc')
-
-g.append('path')
-  .attr('d', arc)
-  .attr('class', 'arc')
-  .style('fill', (d, i) => colorScale(i))
-  .style('stroke', '#11141c')
-  .style('stroke-width', 4)
+  const data = [35, 15, 15, 35]
+  const div = d3.select('body')
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const radius = Math.min(width, height) / 2
+  const colorScale = d3.scaleOrdinal(['#7326AB', '#2A59A9', '#E5A1D4', '#00A0B0'])
+  const svg = div.append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`)
+  
+  const pie = d3.pie().value(d => d).sort(null)
+  const arc = d3.arc().outerRadius(radius * 0.75).innerRadius(0)
+  
+  const g = svg.selectAll('.arc')
+    .data(pie(data))
+    .enter().append('g')
+    .attr('class', 'arc')
+  
+  g.append('path')
+    .attr('d', arc)
+    .attr('class', 'arc')
+    .style('fill', (d, i) => colorScale(i))
+    .style('stroke', '#11141C')
+    .style('stroke-width', 4)
 }
 makeChart()
