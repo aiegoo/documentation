7ce41b05117b1745a231f928ca03f0d335bf3af6
aiegoo <eozz21@gmail.com>
2022-04-04 15:06:27 +0900

    add qt burgermenu file

---
+++ qt/burgermenu.cpp
@@ -0,0 +1,23 @@
+#include "burgermenu.h"
+#include <QStackWidget>
+#include <QVboxLayout>
+#include <QHboxLayout>
+#include <QPushButton>
+#include <QAction>
+#include <QString>
+#include <QStyleOption>
+#include <QPainter>
+#include <QPropertyAnimation>
+#include <QLabel>
+
+
+static const QString BurgerButtonObjectName("BurgerButton");
+static const QString BurgerMenuName("BurgerMenu");
+static const QString MainBurgerButtonOjectname("MainBurgerButton");
+
+class BurgerButton : public QPushButton
+{
+     public:
+     BurgerButton(Qaction+ Action, QWidget+ parent)
+     : QPushButton(parent)
+}
